808bd25fe23a285de67a633b6ebbd9bb46d1920e
Rozzak <rozzak2009@users.noreply.github.com>
2026-04-14 06:34:14 -0700

    feat: update member list from user request

--- data.js
+++ data.js
@@ -43,14 +43,76 @@ const tiktokMembers = [
 
     // --- MEMBER (Akan di-sorting otomatis berdasarkan followers) ---
     {
-        link: "https://www.tiktok.com/@?_r=1&_t=ZS-95WMzoE1t9k",
+        link: "https://www.tiktok.com/@machinegunnovass0",
         role: "member"
     },
     {
-        link: "https://www.tiktok.com/@member.rookie",
+        link: "https://www.tiktok.com/@ridho_kancut_batman?_r=1&_t=ZS-95WLvnYOXll",
+        role: "member"
+    },
+    {
+        link: "https://www.tiktok.com/@gak__asik",
+        role: "member"
+    },
+    {
+        link: "https://www.tiktok.com/@capturne",
+        role: "member"
+    },
+    {
+        link: "https://www.tiktok.com/@presetdanang?_r=1&_t=ZS-95WN5j04B3X",
+        role: "member"
+    },
+    {
+        link: "https://www.tiktok.com/@verrajhhh",
+        role: "member"
+    },
+    {
+        link: "https://www.tiktok.com/@ramztyty?_r=1&_t=ZS-95WPRHlAjuH",
+        role: "member"
+    },
+    {
+        link: "https://www.tiktok.com/@fenrizedity",
+        role: "member"
+    },
+    {
+        link: "https://vm.tiktok.com/ZS9LfTkPmqbY6-ga6z4/",
+        role: "member"
+    },
+    {
+        link: "https://www.tiktok.com/@loxyeetralalelo",
+        role: "member"
+    },
+    {
+        link: "https://www.tiktok.com/@danmekdi",
+        role: "member"
+    },
+    {
+        link: "https://www.tiktok.com/@fyvzhensem",
+        role: "member"
+    },
+    {
+        link: "https://www.tiktok.com/@zayssangatmalas",
+        role: "member"
+    },
+    {
+        link: "https://www.tiktok.com/@iann.not.sepuh?_r=1&_t=ZS-95Wtlbq5Nj7",
+        role: "member"
+    },
+    {
+        link: "https://www.tiktok.com/@puputdisini.1?_r=1&_t=ZS-95WwZr9E9FE",
+        role: "member"
+    },
+    {
+        link: "https://www.tiktok.com/@facry00?_r=1&_t=ZS-95X4mcVVTxV",
+        role: "member"
+    },
+    {
+        link: "https://www.tiktok.com/@manzypreset",
         role: "member"
     }
-];const talentedMembers = [
+];
+
+const talentedMembers = [
     {
         name: "Skyla Music",
         talent: "Music Producer",
